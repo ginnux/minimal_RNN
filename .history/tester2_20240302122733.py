96d934a94415eebcab192c1f69d638762c9c9648
ginnux's main computer <coldwarrior@163.com>
2024-03-02 12:39:15 +0800

    1.2.0

--- .history/tester2_20240302122733.py
+++ .history/tester2_20240302122733.py
@@ -2,7 +2,7 @@ import torch
 from model import MinimalRNN
 
 # (Num, Length, inputsize)
-a = torch.randn((48, 1484)).to("cuda")
+a = torch.randn((64, 48, 1484)).to("cuda")
 net = MinimalRNN(1484, 8, 8, num_layers=6, device="cuda")
 out = net(a, None)
 print(out[0].shape)
